53eeccff519ae403b59078e17410c322b0f76f88
LangerTwo <adrangel95@gmail.com>
2025-04-08 12:50:02 -0500

    add translation home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
     <nav className="bg-gray-800 text-white px-6 py-4 flex justify-between">
       <h1 className="text-xl font-bold">Mi Portafolio</h1>
       <ul className="flex space-x-4">
-        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/">{t('home')}</Link></li>
         <li><Link to="/about">{t('about')}</Link></li>
         <li><Link to="/projects">{t('projects')}</Link></li>
         <li><Link to="/contact">{t('contact')}</Link></li>
